468a7b43910cfc0789e4a78611847bbdee0d494f
Ricardo Guardiola <ricardoguardiolahn@gmail.com>
2026-02-26 02:53:37 -0600

    fix: ThemeProvider not saving theme

--- app/lib/ThemeProvider.tsx
+++ app/lib/ThemeProvider.tsx
@@ -1,17 +1,30 @@
 "use client";
 
 import { Theme } from "@radix-ui/themes";
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { ThemeContextType, Appearance } from "../interfaces/interface";
 
 const ThemeContext = createContext<ThemeContextType | null>(null);
 
 export function ThemeProvider({ children }: { children: React.ReactNode }) {
   const [appearance, setAppearance] = useState<Appearance>("light");
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    // Leer el valor del localStorage cuando el componente se monta
+    const storedAppearance = localStorage.getItem("appearance") as Appearance;
+    if (storedAppearance) {
+      setAppearance(storedAppearance);
+    }
+    setMounted(true);
+  }, []);
 
   const toggleTheme = () => {
-    setAppearance((prev) => (prev === "light" ? "dark" : "light"));
-    localStorage.setItem("appearance", appearance);
+    setAppearance((prev) => {
+      const newAppearance = prev === "light" ? "dark" : "light";
+      localStorage.setItem("appearance", newAppearance);
+      return newAppearance;
+    });
   };
 
   return (
